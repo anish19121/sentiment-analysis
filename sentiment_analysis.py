83fca2e7d87c710d96f4d8c2d186edf1e86f5c3e
anish19121 <anish.kalisetti@gmail.com>
2023-11-28 14:07:24 -0500

    Add sentiment-analysis submodule

---
+++ sentiment_analysis.py
@@ -0,0 +1,11 @@
+# Import necessary libraries
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score, classification_report
+
+# Load the IMDb dataset
+df = pd.read_csv('C:\Users\anish\OneDrive\Desktop\IMDB Dataset.csv')
+
+# Continue with the rest of your code...
